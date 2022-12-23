109b8617aba82c05169f1dc1b7d1e56a1530721f
Jerin1406 <79559253+Jerin1406@users.noreply.github.com>
2022-12-23 13:28:37 +0530

    first Commit

---
+++ springboot_ms/src/main/java/com/task/springboot_ms/SpringbootMsApplication.java
@@ -0,0 +1,13 @@
+package com.task.springboot_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMsApplication.class, args);
+	}
+
+}
---
+++ springboot_ms/src/test/java/com/task/springboot_ms/SpringbootMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.springboot_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
